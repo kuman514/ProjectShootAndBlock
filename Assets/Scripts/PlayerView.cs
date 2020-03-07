826ae89b5c29b9d48448888093630b5cd1895eca
kumankoishi.py <7iggif@naver.com>
2020-03-07 16:38:17 +0900

    Player's View can rotate without its own body tilted

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 [AddComponentMenu("Camera-Control/Smooth Mouse Look")]
 public class PlayerView : MonoBehaviour
 {
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
+	// Settings
 	public float sensitivityX = 15F;
 	public float sensitivityY = 15F;
 
@@ -16,6 +15,9 @@ public class PlayerView : MonoBehaviour
 	public float minimumY = -60F;
 	public float maximumY = 60F;
 
+	public float frameCounter = 20;
+
+	// Inner Active
 	float rotationX = 0F;
 	float rotationY = 0F;
 
@@ -25,98 +27,56 @@ public class PlayerView : MonoBehaviour
 	private List<float> rotArrayY = new List<float>();
 	float rotAverageY = 0F;
 
-	public float frameCounter = 20;
-
 	Quaternion originalRotation;
+
+	// Component References
+	public Camera cam;
 
 	void Update()
 	{
-		if (axes == RotationAxes.MouseXAndY)
+		// Horizontal View Rotation: Player's Main Body
+		rotAverageX = 0f;
+
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+		rotArrayX.Add(rotationX);
+
+		if (rotArrayX.Count >= frameCounter)
 		{
-			rotAverageY = 0f;
-			rotAverageX = 0f;
-
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-
-			rotArrayY.Add(rotationY);
-			rotArrayX.Add(rotationX);
-
-			if (rotArrayY.Count >= frameCounter)
-			{
-				rotArrayY.RemoveAt(0);
-			}
-			if (rotArrayX.Count >= frameCounter)
-			{
-				rotArrayX.RemoveAt(0);
-			}
-
-			for (int j = 0; j < rotArrayY.Count; j++)
-			{
-				rotAverageY += rotArrayY[j];
-			}
-			for (int i = 0; i < rotArrayX.Count; i++)
-			{
-				rotAverageX += rotArrayX[i];
-			}
-
-			rotAverageY /= rotArrayY.Count;
-			rotAverageX /= rotArrayX.Count;
-
-			rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
-			rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
-
-			Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
-			Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
-
-			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+			rotArrayX.RemoveAt(0);
 		}
-		else if (axes == RotationAxes.MouseX)
+		for (int i = 0; i < rotArrayX.Count; i++)
 		{
-			rotAverageX = 0f;
-
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-
-			rotArrayX.Add(rotationX);
-
-			if (rotArrayX.Count >= frameCounter)
-			{
-				rotArrayX.RemoveAt(0);
-			}
-			for (int i = 0; i < rotArrayX.Count; i++)
-			{
-				rotAverageX += rotArrayX[i];
-			}
-			rotAverageX /= rotArrayX.Count;
-
-			rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
-
-			Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
-			transform.localRotation = originalRotation * xQuaternion;
+			rotAverageX += rotArrayX[i];
 		}
-		else
+		rotAverageX /= rotArrayX.Count;
+
+		rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+		Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+		transform.localRotation = originalRotation * xQuaternion;
+
+		// Vertical View Rotation: Camera On Player
+		rotAverageY = 0f;
+
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+		rotArrayY.Add(rotationY);
+
+		if (rotArrayY.Count >= frameCounter)
 		{
-			rotAverageY = 0f;
-
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-
-			rotArrayY.Add(rotationY);
-
-			if (rotArrayY.Count >= frameCounter)
-			{
-				rotArrayY.RemoveAt(0);
-			}
-			for (int j = 0; j < rotArrayY.Count; j++)
-			{
-				rotAverageY += rotArrayY[j];
-			}
-			rotAverageY /= rotArrayY.Count;
-
-			rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
-
-			Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
-			transform.localRotation = originalRotation * yQuaternion;
+			rotArrayY.RemoveAt(0);
 		}
+		for (int j = 0; j < rotArrayY.Count; j++)
+		{
+			rotAverageY += rotArrayY[j];
+		}
+		rotAverageY /= rotArrayY.Count;
+
+		rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+
+		Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
+		cam.transform.localRotation = originalRotation * yQuaternion;
 	}
 
 	void Start()
