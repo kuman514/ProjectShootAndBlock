a849a6f91520d4927890b3b0b0aeda08f98d6831
kumankoishi.py <7iggif@naver.com>
2020-03-13 16:49:24 +0900

    Implementing Grenade Detonation

---
+++ Assets/Scripts/GrenadeDetonation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeDetonation : MonoBehaviour
+{
+    // Detonation Specification
+    public float secondsToDetonate;
+    public float damageAtCenter;
+    public float damageRange;
+    public float knockbackAtCenter;
+
+    // Inner Active
+    private GameObject objectToDetonate;
+
+    // Component Reference
+    public GameObject detonationEffect;
+    public AudioClip detonationSound;
+    public AudioSource detonationSpeaker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectToDetonate = this.transform.gameObject;
+        Debug.Log(objectToDetonate.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReduceLifeSpan();
+    }
+
+    void ReduceLifeSpan()
+    {
+        secondsToDetonate -= Time.deltaTime;
+
+        if(secondsToDetonate <= 0)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Destroy(objectToDetonate);
+    }
+}
