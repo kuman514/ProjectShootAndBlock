e46bc8f89585f22fd70002acec76c4fd5ead78c1
kumankoishi.py <7iggif@naver.com>
2020-03-14 14:08:37 +0900

    Implementing Enemy Attack Methods

---
+++ Assets/Scripts/EnemyAttacks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttacks : MonoBehaviour
+{
+    // Enemy Attack Specification
+    // None Yet
+
+    // Inner Active
+    // None Yet
+
+    // Component References
+    public Transform attackSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRaycastAttack()
+    {
+
+    }
+
+    public void SpawnProjectileAttack()
+    {
+
+    }
+}
