f22f75f40c9a5df1b9fcb4f838466341d85b45e3
kumankoishi.py <7iggif@naver.com>
2020-04-12 12:15:08 +0900

    Implementing Guided Missile

---
+++ Assets/Scripts/ShieldMissile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMissile : MonoBehaviour
+{
+    // Missile Specification
+    public float speed;
+
+    // Inner Active
+    // None Yet
+
+    // Component References
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
