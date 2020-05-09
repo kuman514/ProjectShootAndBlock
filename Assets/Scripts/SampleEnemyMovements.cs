37fb9094947b38535b4b97c880f29d22948d79b3
kumankoishi.py <7iggif@naver.com>
2020-05-09 21:25:06 +0900

    Added Enemies Movements

---
+++ Assets/Scripts/SampleEnemyMovements.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleEnemyMovements : MonoBehaviour
+{
+    // Specification
+    public float force;
+
+    // Inner Active
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(Move());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Move()
+    {
+        while(true)
+        {
+            float dir1 = Random.Range(-1f, 1f);
+            float dir2 = Random.Range(-1f, 1f);
+            float dirV = Random.Range(0, 3f);
+
+            yield return new WaitForSeconds(1);
+            rb.velocity = new Vector3(dir1, dirV, dir2) * force;
+        }
+    }
+}
