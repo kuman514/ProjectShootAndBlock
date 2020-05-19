671a8f239bd5a250d05199d10634ea46e4a51f92
kumankoishi.py <7iggif@naver.com>
2020-05-19 23:12:03 +0900

    Implementing Heal Plate

---
+++ Assets/Scripts/HealPlateMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPlateMethods : MonoBehaviour
+{
+    // specification
+    public float healPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(!collision.transform.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        HealthManager playerHP = collision.transform.gameObject.GetComponent<HealthManager>();
+        if(playerHP != null)
+        {
+            playerHP.Heal(healPerSecond * Time.deltaTime);
+        }
+    }
+}
