e96ea7965a88a8448f7d8723fa2a36b71009f587
kumankoishi.py <7iggif@naver.com>
2020-06-06 21:29:10 +0900

    Implemented sound of gate & elevator

--- Assets/Scripts/InteractiveElevatingObject.cs
+++ Assets/Scripts/InteractiveElevatingObject.cs
@@ -10,11 +10,16 @@ public class InteractiveElevatingObject : MonoBehaviour
 
     // Inner Active
     int destFloor;
+    AudioSource sound;
+
+    // Component References
+    public AudioClip soundEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         destFloor = 0;
+        sound = this.transform.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +33,11 @@ public class InteractiveElevatingObject : MonoBehaviour
         if (0 <= dest && dest < destinationY.Length)
         {
             destFloor = dest;
+
+            if (sound != null && soundEffect != null)
+            {
+                sound.PlayOneShot(soundEffect);
+            }
         }
     }
 
