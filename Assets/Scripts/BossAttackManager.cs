aba57a4be48da702d2027ad12e3d3c66311a3041
kumankoishi.py <7iggif@naver.com>
2020-06-11 04:28:46 +0900

    Implementing Boss Attack

---
+++ Assets/Scripts/BossAttackManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackManager : MonoBehaviour
+{
+    // Enemy Attack Specification
+    public float attackTerm;
+
+    // Inner Active
+    private EnemyAttacks ea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ea = this.transform.GetComponent<EnemyAttacks>();
+        StartCoroutine(Attack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Attack()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(attackTerm);
+            ea.SpawnProjectileAttack();
+        }
+    }
+}
